a5d954dbec548853158fa3a70cc4aa1cbda188e8
urmilp <40172150+urmilp@users.noreply.github.com>
2018-06-13 16:22:03 +0530

    Remove Unnecessary Code from wordpress
    Remove Unnecessary Code from wp_head

---
+++ functions.php
@@ -0,0 +1,8 @@
+/**** Remove Unnecessary Code from wp_head***/
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'start_post_rel_link');
+remove_action('wp_head', 'index_rel_link');
+remove_action('wp_head', 'adjacent_posts_rel_link');
+/**** Remove Unnecessary Code from wp_head end***/
